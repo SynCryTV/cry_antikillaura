e55db4584e604baf268a3cde4fbc3778ed839768
SynCryTV <syncryxx@gmail.com>
2025-04-01 07:23:52 +0200

    Prevent to deny double count of kills

--- c.lua
+++ c.lua
@@ -1,5 +1,5 @@
 local killLog = {}
-
+local checkedDeaths = {}
 
 Citizen.CreateThread(function()
     while true do
@@ -9,10 +9,19 @@ Citizen.CreateThread(function()
         for _, playerId in ipairs(GetActivePlayers()) do
             local targetPed = GetPlayerPed(playerId)
             if DoesEntityExist(targetPed) and IsPedAPlayer(targetPed) then
-                local killer = GetPedSourceOfDeath(targetPed)
-
-                if killer == playerPed then
-                    trackKill()
+                local targetServerId = GetPlayerServerId(playerId)
+                
+                if IsEntityDead(targetPed) then
+                    local killer = GetPedSourceOfDeath(targetPed)
+                    
+                    if killer == playerPed and not checkedDeaths[targetServerId] then
+                        checkedDeaths[targetServerId] = true
+                        trackKill()
+                        
+                        Citizen.SetTimeout(30000, function()
+                            checkedDeaths[targetServerId] = nil
+                        end)
+                    end
                 end
             end
         end
@@ -21,14 +30,14 @@ end)
 
 function trackKill()
     local currentTime = GetGameTimer()
-
+    
+    table.insert(killLog, currentTime)
+    
     for i = #killLog, 1, -1 do
         if (currentTime - killLog[i]) > 500 then
             table.remove(killLog, i)
         end
     end
-
-    table.insert(killLog, currentTime)
 
     if #killLog >= 3 then
         TriggerServerEvent("anticheat:playerKillWarning")
